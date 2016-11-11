05061a03f2613ed66af9cab7a3746d3a2f692c8d
sol <sol993>
2016-11-11 04:24:11 -0300

    Quedo hasta acá

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <link href="//netdna.bootstrapcdn.com/font-awesome/3.1.1/css/font-awesome.css" rel="stylesheet">
 <link rel="stylesheet" type="text/css" href="estilo/estilo.css">
 <div class="testbox">
-  <h1>Registration</h1>
+  <h1>Ingresar</h1>
   <title></title>
 </head>
 <body>
@@ -14,26 +14,24 @@
       <input type="radio" value="None" id="radioOne" name="account" checked/>
       <label for="radioOne" class="radio" chec>Personal</label>
       <input type="radio" value="None" id="radioTwo" name="account" />
-      <label for="radioTwo" class="radio">Company</label>
+      <label for="radioTwo" class="radio">Admin</label>
     </div>
   <hr>
-  <label id="icon" for="name"><i class="icon-envelope "></i></label>
-  <input type="text" name="name" id="name" placeholder="Email" required/>
+  <!--<label id="icon" for="name"><i class="icon-envelope "></i></label>
+  <input type="text" name="name" id="name" placeholder="Email" required/> -->
   <label id="icon" for="name"><i class="icon-user"></i></label>
-  <input type="text" name="name" id="name" placeholder="Name" required/>
+  <input type="text" name="name" id="name" placeholder="Nombre" required/>
   <label id="icon" for="name"><i class="icon-shield"></i></label>
-  <input type="password" name="name" id="name" placeholder="Password" required/>
+  <input type="password" name="name" id="name" placeholder="Contrasenia" required/>
   <div class="gender">
-    <input type="radio" value="None" id="male" name="gender" checked/>
-    <label for="male" class="radio" chec>Male</label>
+   <!-- <input type="radio" value="None" id="male" name="gender" checked/>
+    <label for="male" class="radio" chec>Male</label> -->
     <input type="radio" value="None" id="female" name="gender" />
-    <label for="female" class="radio">Female</label>
+    <label for="female" class="radio">Recordame</label>
    </div> 
-   <p>By clicking Register, you agree on our <a href="#">terms and condition</a>.</p>
-   <a href="#" class="button">Register</a>
+   <input  type="button" onclick="MostarLogin()"   value="Ingresar" title="Registra tu cuenta">
+   <!--  <a href="#" class="button">Register</a>-->
   </form>
 </div>
 </body>
 </html>
-
-
